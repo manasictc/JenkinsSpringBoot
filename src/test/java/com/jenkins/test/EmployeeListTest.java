bb4bca533a5f9175ebbb8bed0b0b73632a26b1c1
manasictc <manasijena09@gmail.com>
2023-10-07 19:15:48 +0530

    test case with new test filey

---
+++ src/test/java/com/jenkins/test/EmployeeListTest.java
@@ -0,0 +1,36 @@
+package com.jenkins.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class EmployeeListTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testEmployeeName() {
+		String str = "manas";
+       assertEquals("manasi", str);
+
+	}
+
+}
